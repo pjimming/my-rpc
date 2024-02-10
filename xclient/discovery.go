37cf90b264d1e87f7b9fda0ef9eabf77edbd6546
pan <2556323541@qq.com>
2024-02-10 21:13:35 +0800

    实现负载均衡

---
+++ xclient/discovery.go
@@ -0,0 +1,84 @@
+package xclient
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+type SelectMode int
+
+const (
+	RandomSelect SelectMode = iota
+	RoundRobinSelect
+)
+
+type Discovery interface {
+	Refresh() error // refresh from remote registry
+	Update(servers []string) error
+	Get(mode SelectMode) (string, error)
+	GetAll() ([]string, error)
+}
+
+// MultiServersDiscovery is a discovery for multi servers without a registry center
+// user provides the server addresses explicitly instead
+type MultiServersDiscovery struct {
+	r       *rand.Rand   // generate random number
+	mu      sync.RWMutex // protect following
+	servers []string
+	index   int // record the selected position for robin algorithm
+}
+
+// NewMultiServersDiscovery creates a MultiServersDiscovery instance
+func NewMultiServersDiscovery(servers []string) *MultiServersDiscovery {
+	d := &MultiServersDiscovery{
+		servers: servers,
+		r:       rand.New(rand.NewSource(time.Now().UnixNano())),
+	}
+
+	d.index = d.r.Intn(math.MaxInt32 - 1)
+	return d
+}
+
+var _ Discovery = (*MultiServersDiscovery)(nil)
+
+func (d *MultiServersDiscovery) Refresh() error {
+	return nil
+}
+
+func (d *MultiServersDiscovery) Update(servers []string) error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	d.servers = servers
+	return nil
+}
+
+func (d *MultiServersDiscovery) Get(mode SelectMode) (string, error) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	n := len(d.servers)
+	if n <= 0 {
+		return "", fmt.Errorf("rpc discovery: no available servers")
+	}
+
+	switch mode {
+	case RandomSelect:
+		return d.servers[d.r.Intn(n)], nil
+	case RoundRobinSelect:
+		s := d.servers[d.index%n]
+		d.index = (d.index + 1) % n
+		return s, nil
+	default:
+		return "", fmt.Errorf("rpc discovery: not supported mode")
+	}
+}
+
+func (d *MultiServersDiscovery) GetAll() ([]string, error) {
+	d.mu.RLock()
+	defer d.mu.RUnlock()
+	servers := make([]string, len(d.servers), len(d.servers))
+	copy(servers, d.servers)
+	return servers, nil
+}
